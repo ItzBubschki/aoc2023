e899bcbc3ec78a03060e85b52a2e8d9cc2de6a9a
Laurin Niemeyer <laurin@niemeyer.name>
2023-12-08 12:47:07 +0100

    minor refactoring for day 8;

--- src/com/itzbubschki/aoc2023/day08/Day08.kt
+++ src/com/itzbubschki/aoc2023/day08/Day08.kt
@@ -13,58 +13,56 @@ fun main() {
     part2().println()
 }
 
-fun part1(): Int {
-    val instructionParts = input.joinToString("\n").split("\n\n")
-    val instructions = instructionParts.first()
-    val nodeInstructions = instructionParts.last().split("\n")
-    var current = "AAA"
-    var currentStep = Pair(instructions.first(), 0)
-    var steps = 0
-    while (current != "ZZZ") {
-        val line = nodeInstructions.find { it.startsWith(current) }
-        val leftAndRight = getLeftAndRight(line!!)
-        current = when (currentStep.first) {
-            'L' -> leftAndRight.first()
-            else -> leftAndRight.last()
-        }
-        val nextInstruction = instructions.getOrNull(currentStep.second + 1) ?: instructions[0]
-        currentStep =
-            Pair(nextInstruction, if (currentStep.second + 1 >= instructions.length) 0 else currentStep.second + 1)
-        steps++
-    }
-    return steps
+fun part1(): Long {
+    val (instructions, nodeInstructions) = extractInstruction()
+
+    return processInstructions(instructions, nodeInstructions, "AAA").second
 }
 
 fun part2(): Long {
-    val input = readInput("Day08")
+    val (instructions, nodeInstructions) = extractInstruction()
+
+    val starterNodes =
+        nodeInstructions.filter { it.substringBefore(" =").endsWith("A") }.map { it.substringBefore(" =") }
+    val stepsToStart = mutableListOf<Long>()
+
+    starterNodes.forEach { node ->
+        val result = processInstructions(instructions, nodeInstructions, node)
+        stepsToStart.add(result.second)
+    }
+
+    return stepsToStart.reduce { acc, it -> acc.lcm(it) }
+}
+
+fun processInstructions(instructions: String, nodeInstructions: List<String>, current: String): Pair<String, Long> {
+    var currentNode = current
+    var currentInstruction = Pair(instructions.first(), 0)
+    var steps = 0L
+    while (!currentNode.endsWith("Z")) {
+        nodeInstructions.find { it.startsWith(currentNode) }.let { line ->
+            val leftAndRight =
+                getLeftAndRight(line ?: throw IllegalArgumentException("No line found starting with $currentNode"))
+            currentNode = if (currentInstruction.first == 'L') {
+                leftAndRight.first()
+            } else {
+                leftAndRight.last()
+            }
+            val nextInstruction = instructions.getOrNull(currentInstruction.second + 1) ?: instructions[0]
+            currentInstruction = Pair(
+                nextInstruction,
+                if (currentInstruction.second + 1 >= instructions.length) 0 else currentInstruction.second + 1
+            )
+            steps++
+        }
+    }
+    return Pair(currentNode, steps)
+}
+
+fun extractInstruction(): Pair<String, List<String>> {
     val instructionParts = input.joinToString("\n").split("\n\n")
     val instructions = instructionParts.first()
     val nodeInstructions = instructionParts.last().split("\n")
-
-    val starterNodes = nodeInstructions.filter { it.substringBefore(" =").endsWith("A") }.map { it.substringBefore(" =") }
-    val stepsToStart = mutableListOf<Long>()
-
-    starterNodes.mapIndexed { index, node ->
-        var current = node
-        var steps = 0L
-        var currentInstruction = Pair(instructions.first(), 0)
-        while (!current.endsWith("Z")) {
-            val line = nodeInstructions.find { it.startsWith(current) }
-            val leftAndRight = getLeftAndRight(line!!)
-            current = when(currentInstruction.first) {
-                'L' -> leftAndRight.first()
-                else -> leftAndRight.last()
-            }
-            val nextInstruction = instructions.getOrNull(currentInstruction.second+1) ?: instructions[0]
-            currentInstruction = Pair(nextInstruction, if (currentInstruction.second+1 >=instructions.length) 0 else currentInstruction.second+1)
-            steps++
-        }
-        stepsToStart.add(steps)
-    }
-    //return the roduct of al members in stepsToStart
-    return stepsToStart.reduce { acc, it ->
-        acc.lcm(it)
-    }
+    return Pair(instructions, nodeInstructions)
 }
 
 fun getLeftAndRight(line: String): List<String> {
